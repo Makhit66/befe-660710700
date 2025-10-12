429eb79dcbe01146d1f8e4e33bf4ee74359446b7
Chayanon sukain <sukain_s@su.ac.th>
2025-10-12 06:52:38 +0000

    week10-lab

--- week9-lab1/main.go
+++ week9-lab1/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 	"github.com/gin-gonic/gin"
 	_"github.com/lib/pq"
+    "github.com/gin-contrib/cors"
 )
 
 type Book struct {
@@ -200,6 +201,8 @@ func deleteBook(c *gin.Context) {
 	initDB()
 	defer db.Close()
 	r := gin.Default()
+
+    r.Use(cors.Default())
 
 	r.GET("/health", func(c *gin.Context){
 		err := db.Ping()
